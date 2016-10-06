e5fa7b574f973dd19e9742143efc1bb0d50dcbeb
kikollovet <kikollovet@gmail.com>
2016-10-06 16:05:29 -0300

    Update Empresa.java

--- BrincandoComScanner/src/Empresa.java
+++ BrincandoComScanner/src/Empresa.java
@@ -9,4 +9,12 @@ public class Empresa {
 		return _listaFuncionarios;
 	}
 	
+	public boolean usuarioJaCadastrado(String nome){
+		for(Funcionario funcionario: _listaFuncionarios){
+			if(funcionario.get_nome().equals(nome)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
